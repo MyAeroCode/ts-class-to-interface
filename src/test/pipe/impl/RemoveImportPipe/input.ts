2b756d28f27b3c35579e2d461ffd45c9aa63d6e4
Lutz <meteo_meteo@naver.com>
2022-10-07 18:26:17 +0900

    feat: implement RemoveImportPipe (#5)

---
+++ src/test/pipe/impl/RemoveImportPipe/input.ts
@@ -0,0 +1,19 @@
+("CASE 1");
+import fs1 from "fs";
+
+("CASE 2");
+import * as fs2 from "fs";
+
+("CASE 3");
+import { read } from "fs";
+
+("CASE 4");
+import {
+  watch,
+  watchFile,
+  write,
+  writeFile,
+  writeFileSync,
+  writeSync,
+  writev,
+} from "fs";
