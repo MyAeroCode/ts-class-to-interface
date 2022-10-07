339ee7b87042d8d9ae2a5f01eb98e57f31053841
Lutz <meteo_meteo@naver.com>
2022-10-07 18:13:54 +0900

    chore: remove unused files

--- src/core/pipe/impl/EchoBackPipe.ts
+++
@@ -1,7 +0,0 @@
-import { Pipe, IPipe } from "@core/pipe/Pipe";
-
-export class EchoBackPipe extends Pipe implements IPipe {
-  protected processImpl(input: string): string {
-    return input;
-  }
-}
--- src/test/hello.test.ts
+++
@@ -1,7 +0,0 @@
-import { helloWorld } from "@core/hello";
-
-describe("hello", () => {
-  it("should return hello", () => {
-    expect(helloWorld()).toBe("Hello, World!");
-  });
-});
